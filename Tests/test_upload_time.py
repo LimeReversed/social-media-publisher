acf67c770f0b08ea136a2db78361ea049a476a4b
Lime Reversed <limereversed@pm.me>
2026-06-12 12:36:48 +0200

    Fixed tests

--- Tests/test_upload_time.py
+++ Tests/test_upload_time.py
@@ -24,3 +24,12 @@ class TimeTests(TestCase):
         result = next_upload_datetime(upload_time, now)
 
         self.assertEqual(expected, result)
+
+    def test_next_upload__upload_time_is_today_but_after_release_time__should_go_to_next_week(self):
+        now = datetime.datetime(2026, 6, 8, 12, 0)  # Monday an hour after upload time
+        upload_time = UploadTime(day=0, hour=11, minute=0)  # Monday at 11:00
+        expected = datetime.datetime(2026, 6, 15, 11, 0)
+
+        result = next_upload_datetime(upload_time, now)
+
+        self.assertEqual(expected, result)
